83a56c4396ff42ccd146a6fa26ef90f58c6fe556
Hardik Arora <hardikarora826@gmail.com>
2021-07-02 18:42:46 +0530

    added contact page
    added contact page and features

--- app.js
+++ app.js
@@ -7,15 +7,37 @@ const homeSec= document.querySelector(".home-sec")
 const workSec= document.querySelector(".work-sec")
 const contactSec= document.querySelector(".contact-sec")
 
+const homeBtnActive= document.querySelector(".fa-home")
+const workBtnActive= document.querySelector(".fa-folder")
+const contactBtnActive= document.querySelector(".fa-envelope")
+
 homeBtn.addEventListener("click", function(){
     homeSec.style.display= "flex"
     workSec.style.display= "none"
+    contactSec.style.display="none"
+    // homeBtnActive.style.color= "white"
+    // workBtnActive.style.color="rgba(240, 255, 255, 0.692)"
+    // contactBtnActive.style.color="rgba(240, 255, 255, 0.692)"
+
 })
 
 
 
 workBtn.addEventListener("click", function(){
     homeSec.style.display= "none"
-    // contactSec.style.display="none"
+    contactSec.style.display="none"
     workSec.style.display= "flex"
+    // workBtnActive.style.color= "white"
+    // homeBtnActive.style.color= "rgba(240, 255, 255, 0.692)" 
+    // contactBtnActive.style.color= "rgba(240, 255, 255, 0.692)"
+})
+
+
+contactBtn.addEventListener("click", function(){
+    homeSec.style.display= "none"
+    contactSec.style.display="flex"
+    workSec.style.display= "none"
+    // homeBtnActive.style.color= "rgba(240, 255, 255, 0.692)"
+    // workBtnActive.style.color= "rgba(240, 255, 255, 0.692)"
+    // contactBtnActive.style.color= "white"
 })
